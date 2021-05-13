dc89f318f44d2e72a5febb4b4ea28957d47f941c
mohamedyahyajday <mohamedyahya.jday@esprit.tn>
2021-05-13 03:53:33 +0100

    ajouterproduit champ genre

--- view/ajouterproduit1.php
+++ view/ajouterproduit1.php
@@ -91,7 +91,7 @@ try{
     </head>
    <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <img src="logo.png" alt="" height="150" width="150" href="index.html" >
+            <img src="../logo.png" alt="" height="150" width="150" href="index.html" >
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
@@ -270,7 +270,7 @@ try{
 
 <?php
 require_once('../connection.php');
-$mysqli = new mysqli('localhost', 'root', '' ,'yahya');
+$mysqli = new mysqli('localhost', 'root', '' ,'web');
 if($mysqli->connect_error){
     die('Connect-Error (' . $mysqli->connect_error . ') '
         . $mysqli->connect_error);
