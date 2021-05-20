3908e51bf855bc5bec797200fd2cd2c3648db144
arij.hajji@esprit.tn <arij.hajji@esprit.tn>
2021-05-20 12:12:22 +0100

    vv

--- view/afficherproduitfront.php
+++ view/afficherproduitfront.php
@@ -254,6 +254,8 @@ $wishliste = new wishliste($id_user,$_POST['ID']);
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../css/session.css">
+    
+
 </head>
 
 <body>
